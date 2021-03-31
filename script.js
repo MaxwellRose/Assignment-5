716f7761d6036bb1bdec14dd5b0d386326a2c8c4
Maxwell Rose <maxwell.rose@ocsbstudent.ca>
2021-03-31 12:35:47 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let diameter = 0
+const pi = 3.14
+let circumference = 0
+
+document.getElementById('calculate').addEventListener('click', formula)
+
+function formula () {
+  diameter = document.getElementById('input1').value
+
+  diameter = parseInt(diameter)
+
+  area = pi * (diameter / 2) * (diameter / 2) 
+
+  alert(area)
+}
+
+document.getElementById('calculate1').addEventListener('click', formula1)
+
+function formula1 () {
+  diameter = document.getElementById('input1').value
+
+  diameter = parseInt(diameter)
+
+  circumference = 2 * pi * (diameter / 2) 
+
+  alert(circumference)
+}
+
+
